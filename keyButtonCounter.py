bac6124d92b65e9a7e3a5a29dd19e6c502c7cfea
Zack <izaac.corbett@gmail.com>
2016-12-24 20:15:22 -0600

    counterUpdate: 518

--- keyButtonCounter.py
+++ keyButtonCounter.py
@@ -5,7 +5,7 @@ def AddReplacePush(numberToAdd):
     holla = str(holla)
     print holla
     timeSaved = int(holla) * .5
-    minutes = float(timeSaved) / 600
+    minutes = float(timeSaved) / 60
 
     try:
         with open('buttonCounter.txt', 'r+') as writeNewCounter:
