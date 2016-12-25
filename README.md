f5dbe7ef70fafb4344b2158b717a1860c8c90b13
Zack <izaac.corbett@gmail.com>
2016-12-24 19:43:15 -0600

    counterUpdate: 544

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # shipStationButtonSmashCounter<br>
-            This is a counter for the number of times a custom button is pressed that I made/created/installed at work. <br>
-            Not so much for practicality, it saves only about 1/2 second/press or 1/2 the total keystrokes, but conceptually has broadened my tech aptitude. <br>
-
-            It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
-            As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
-            <br>
-            Keystrokes not pressed: 543 Time saved: 4 minutes
+                This is a counter for the number of times a custom button is pressed that I made/created/installed at work. <br>
+                Not so much for practicality, it saves only about 1/2 second/press or 1/2 the total keystrokes, but conceptually has broadened my tech aptitude. <br>
+                <br>
+                It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
+                As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
+                <br>
+                Keystrokes not pressed: 544<br>
+                Time saved: 4 minutes
--- keyButtonCounter.py
+++ keyButtonCounter.py
@@ -13,17 +13,20 @@ def AddAndReplace(numberToAdd):
     except:
         pass
 
-    with open('README.md', 'w+') as README:
-        README.write(
-            """# shipStationButtonSmashCounter<br>
-            This is a counter for the number of times a custom button is pressed that I made/created/installed at work. <br>
-            Not so much for practicality, it saves only about 1/2 second/press or 1/2 the total keystrokes, but conceptually has broadened my tech aptitude. <br>
-
-            It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
-            As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
-            <br>
-            Keystrokes not pressed: %s Time saved: %s minutes""" % (holla, minutes))
-
+    try:
+        with open('README.md', 'w+') as README:
+            README.write(
+                """# shipStationButtonSmashCounter<br>
+                This is a counter for the number of times a custom button is pressed that I made/created/installed at work. <br>
+                Not so much for practicality, it saves only about 1/2 second/press or 1/2 the total keystrokes, but conceptually has broadened my tech aptitude. <br>
+                <br>
+                It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
+                As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
+                <br>
+                Keystrokes not pressed: %s<br>
+                Time saved: %s minutes""" % (holla, minutes))
+    except:
+        pass
     writeNewCounter.close()
     README.close()
     os.system('git add .')
