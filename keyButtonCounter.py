be462b5ebeb2a476fea803b76ede8c1410cefbd5
Zack <izaac.corbett@gmail.com>
2016-12-24 19:46:37 -0600

    counterUpdate: 552

--- keyButtonCounter.py
+++ keyButtonCounter.py
@@ -17,14 +17,15 @@ def AddReplacePush(numberToAdd):
         with open('README.md', 'w+') as README:
             README.write(
                 """# shipStationButtonSmashCounter<br>
-                This is a counter for the number of times a custom button is pressed that I made/created/installed at work. <br>
-                Not so much for practicality, it saves only about 1/2 second/press or 1/2 the total keystrokes, but conceptually has broadened my tech aptitude. <br>
-                <br>
-                It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
-                As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
-                <br>
-                Keystrokes not pressed: %s<br>
-                Time saved: %s minutes""" % (holla, minutes))
+This is a counter for the number of times a custom button is pressed that I made/created/installed at work. <br>
+Not so much for practicality, it saves only about 1/2 second/press or 1/2 the total keystrokes, but conceptually has broadened my tech aptitude. <br>
+<br>
+It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
+As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
+<br>
+Keystrokes not pressed: %s<br>
+Time saved: %s minutes""" % (holla, minutes))
+
     except:
         pass
 
