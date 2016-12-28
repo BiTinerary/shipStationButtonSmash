dcd60ad77fa11e2853dc85550f20d6bcffac43b8
Izaac Kirscht <izaac.kirscht@gmail.com>
2016-12-28 10:05:04 -0600

    counterUpdate: 673

--- keyButtonCounter.py
+++ keyButtonCounter.py
@@ -23,8 +23,8 @@ Not so much for practicality, it saves only about 1/2 second/press or 1/2 the to
 It's primary use is just to be more cathartic in smashing a gigantic switch everytime something is sold and shipped. <br>
 As opposed to pressing <kbd>Alt+P</kbd> or swapping from the keyboard to the mouse. <br>
 <br>
-Keystrokes not pressed: **<code>%s</code>**<br>
-Time saved: **<code>%s</code>** minutes""" % (holla, round(minutes, 2)))
+Keystrokes not pressed: **<kbd>%s</kbd>**<br>
+Time saved: **<kbd>%s</kbd>** minutes""" % (holla, round(minutes, 2)))
 
     except:
         pass
