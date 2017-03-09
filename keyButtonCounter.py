f15f69c45cf999ceae8952a637a2b6295c7080d4
Izaac Kirscht <izaac.kirscht@gmail.com>
2017-03-09 12:56:51 -0600

    update

--- keyButtonCounter.py
+++ keyButtonCounter.py
@@ -1,4 +1,4 @@
-import pythoncom, pyHook, os, time, re, pyautogui
+import pythoncom, pyHook, os, time, re, pyautogui, time
 
 def AddReplacePush(numberToAdd):
     holla = int(numberToAdd) + 1
@@ -31,7 +31,7 @@ Time saved: **<kbd>%s</kbd>** minutes""" % (holla, round(minutes, 2)))
 
     writeNewCounter.close()
     README.close()
-    pyautogui.click(1708, 766)
+    pyautogui.typewrite('s')
     os.system('git add .')
     os.system('git commit -m "counterUpdate: %s"' % holla)
     os.system('git push')
